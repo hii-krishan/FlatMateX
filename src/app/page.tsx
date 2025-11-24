a1f2ec1cc0010bc062ce011d34b9b463e53297bb
Krishan Jain <krishanjain0709@gmail.com>
2025-11-24 06:42:47 +0000

    Don't add this login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 'use client';
-import { redirect, useRouter } from 'next/navigation'
-import { useUser } from '@/firebase';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { Loader2 } from 'lucide-react';
 
 export default function Home() {
-  const { user, loading } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading) {
-      if (user) {
-        router.push('/dashboard');
-      } else {
-        router.push('/login');
-      }
-    }
-  }, [user, loading, router]);
+    router.push('/dashboard');
+  }, [router]);
 
   return (
     <div className="flex h-screen items-center justify-center">
